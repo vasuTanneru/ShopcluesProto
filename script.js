d1ca32404345738f9b4c90a36c5ac9c67b02b70b
Vasu Tanneru <vasutanneru035@gmail.com>
2024-04-12 20:07:22 +0530

    0.0.1

---
+++ script.js
@@ -0,0 +1,121 @@
+
+
+function displayCard(){
+    var doc = document.getElementById("display-card1")
+    doc.style.top = "133px";
+    doc.style.visibility = "visible";
+    doc.style.opacity = "1";
+}
+
+function hideCard() {
+    var doc = document.getElementById("display-card1")
+    doc.style.top = "100px";
+    doc.style.visibility = "hidden";
+    doc.style.opacity = "0";
+    // console.log("Hello")
+}
+
+function displayCard2(){
+    var doc = document.getElementById("display-card2")
+    doc.style.top = "133px";
+    doc.style.visibility = "visible";
+    doc.style.opacity = "1";
+}
+
+function hideCard2(){
+    var doc = document.getElementById("display-card2")
+    doc.style.top = "100px";
+    doc.style.visibility = "hidden";
+    doc.style.opacity = "0";
+    // console.log("Hello")
+}
+
+function displayCard3(){
+    var doc = document.getElementById("display-card3")
+    doc.style.top = "133px";
+    doc.style.visibility = "visible";
+    doc.style.opacity = "1";
+}
+
+function hideCard3(){
+    var doc = document.getElementById("display-card3")
+    doc.style.top = "100px";
+    doc.style.visibility = "hidden";
+    doc.style.opacity = "0";
+    // console.log("Hello")
+}
+
+function displayCard4(){
+    var doc = document.getElementById("display-card4")
+    doc.style.top = "133px";
+    doc.style.visibility = "visible";
+    doc.style.opacity = "1";
+}
+
+function hideCard4(){
+    var doc = document.getElementById("display-card4")
+    doc.style.top = "100px";
+    doc.style.visibility = "hidden";
+    doc.style.opacity = "0";
+    // console.log("Hello")
+}
+
+function displayCard5(){
+    var doc = document.getElementById("display-card5")
+    doc.style.top = "133px";
+    doc.style.visibility = "visible";
+    doc.style.opacity = "1";
+}
+
+function hideCard5(){
+    var doc = document.getElementById("display-card5")
+    doc.style.top = "100px";
+    doc.style.visibility = "hidden";
+    doc.style.opacity = "0";
+    // console.log("Hello")
+}
+
+function displayCard6(){
+    var doc = document.getElementById("display-card6")
+    doc.style.top = "133px";
+    doc.style.visibility = "visible";
+    doc.style.opacity = "1";
+}
+
+function hideCard6(){
+    var doc = document.getElementById("display-card6")
+    doc.style.top = "100px";
+    doc.style.visibility = "hidden";
+    doc.style.opacity = "0"
+    // console.log("Hello")
+}
+
+function displayCard7(){
+    var doc = document.getElementById("display-card7")
+    doc.style.top = "133px";
+    doc.style.visibility = "visible";
+    doc.style.opacity = "1";
+}
+
+function hideCard7(){
+    var doc = document.getElementById("display-card7")
+    doc.style.top = "100px";
+    doc.style.visibility = "hidden";
+    doc.style.opacity = "0";
+    // console.log("Hello")
+}
+
+function displayCard8(){
+    var doc = document.getElementById("display-card8")
+    doc.style.top = "133px";
+    doc.style.visibility = "visible";
+    doc.style.opacity = "1";
+}
+
+function hideCard8(){
+    var doc = document.getElementById("display-card8")
+    doc.style.top = "100px";
+    doc.style.visibility = "hidden";
+    doc.style.opacity = "0";
+    // console.log("Hello")
+}
