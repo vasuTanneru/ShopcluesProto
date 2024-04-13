afa4944c114babc5d6fa07d9b22324c5bdc568ac
Vasu Tanneru <vasutanneru035@gmail.com>
2024-04-13 21:57:09 +0530

    updatedCode

--- script.js
+++ script.js
@@ -7,115 +7,198 @@ function displayCard(){
     doc.style.opacity = "1";
 }
 
-function hideCard() {
-    var doc = document.getElementById("display-card1")
-    doc.style.top = "100px";
-    doc.style.visibility = "hidden";
-    doc.style.opacity = "0";
-    // console.log("Hello")
+// function hideCard() {
+//     var doc = document.getElementById("display-card1")
+//     doc.style.top = "100px";
+//     doc.style.visibility = "hidden";
+//     doc.style.opacity = "0";
+//     // console.log("Hello")
+// }
+
+// function displayCard2(){
+//     var doc = document.getElementById("display-card2")
+//     doc.style.top = "133px";
+//     doc.style.visibility = "visible";
+//     doc.style.opacity = "1";
+// }
+
+// function hideCard2(){
+//     var doc = document.getElementById("display-card2")
+//     doc.style.top = "100px";
+//     doc.style.visibility = "hidden";
+//     doc.style.opacity = "0";
+//     // console.log("Hello")
+// }
+
+// function displayCard3(){
+//     var doc = document.getElementById("display-card3")
+//     doc.style.top = "133px";
+//     doc.style.visibility = "visible";
+//     doc.style.opacity = "1";
+// }
+
+// function hideCard3(){
+//     var doc = document.getElementById("display-card3")
+//     doc.style.top = "100px";
+//     doc.style.visibility = "hidden";
+//     doc.style.opacity = "0";
+//     // console.log("Hello")
+// }
+
+// function displayCard4(){
+//     var doc = document.getElementById("display-card4")
+//     doc.style.top = "133px";
+//     doc.style.visibility = "visible";
+//     doc.style.opacity = "1";
+// }
+
+// function hideCard4(){
+//     var doc = document.getElementById("display-card4")
+//     doc.style.top = "100px";
+//     doc.style.visibility = "hidden";
+//     doc.style.opacity = "0";
+//     // console.log("Hello")
+// }
+
+// function displayCard5(){
+//     var doc = document.getElementById("display-card5")
+//     doc.style.top = "133px";
+//     doc.style.visibility = "visible";
+//     doc.style.opacity = "1";
+// }
+
+// function hideCard5(){
+//     var doc = document.getElementById("display-card5")
+//     doc.style.top = "100px";
+//     doc.style.visibility = "hidden";
+//     doc.style.opacity = "0";
+//     // console.log("Hello")
+// }
+
+// function displayCard6(){
+//     var doc = document.getElementById("display-card6")
+//     doc.style.top = "133px";
+//     doc.style.visibility = "visible";
+//     doc.style.opacity = "1";
+// }
+
+// function hideCard6(){
+//     var doc = document.getElementById("display-card6")
+//     doc.style.top = "100px";
+//     doc.style.visibility = "hidden";
+//     doc.style.opacity = "0"
+//     // console.log("Hello")
+// }
+
+// function displayCard7(){
+//     var doc = document.getElementById("display-card7")
+//     doc.style.top = "133px";
+//     doc.style.visibility = "visible";
+//     doc.style.opacity = "1";
+// }
+
+// function hideCard7(){
+//     var doc = document.getElementById("display-card7")
+//     doc.style.top = "100px";
+//     doc.style.visibility = "hidden";
+//     doc.style.opacity = "0";
+//     // console.log("Hello")
+// }
+
+// function displayCard8(){
+//     var doc = document.getElementById("display-card8")
+//     doc.style.top = "133px";
+//     doc.style.visibility = "visible";
+//     doc.style.opacity = "1";
+// }
+
+// function hideCard8(){
+//     var doc = document.getElementById("display-card8")
+//     doc.style.top = "100px";
+//     doc.style.visibility = "hidden";
+//     doc.style.opacity = "0";
+//     // console.log("Hello")
+// }
+
+
+let currentIndex = 0;
+const slides = document.querySelectorAll('.silder_bar img');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.style.display = 'flex';
+        } else {
+            slide.style.display = 'none';
+        }
+    });
 }
 
-function displayCard2(){
-    var doc = document.getElementById("display-card2")
-    doc.style.top = "133px";
-    doc.style.visibility = "visible";
-    doc.style.opacity = "1";
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % totalSlides;
+    showSlide(currentIndex);
 }
 
-function hideCard2(){
-    var doc = document.getElementById("display-card2")
-    doc.style.top = "100px";
-    doc.style.visibility = "hidden";
-    doc.style.opacity = "0";
-    // console.log("Hello")
+showSlide(currentIndex);
+
+setInterval(nextSlide, 2000);
+
+function displayImageCard(val){
+    currentIndex = val
+    nextSlide()
+}
+function hideImageCard1(){
+    var doc=document.getElementsByClassName(".name_hove1")
+    doc.style.visibility="hidden";
+    doc.style.opacity="0";
 }
 
-function displayCard3(){
-    var doc = document.getElementById("display-card3")
-    doc.style.top = "133px";
-    doc.style.visibility = "visible";
-    doc.style.opacity = "1";
+function displayImageCard2(){
+    var doc=document.getElementsByClassName(".name_hove2");
+    doc.style.visibility="visible";
+    doc.style.opacity="1";
+}
+function hideImageCard2(){
+    var doc=document.getElementsByClassName(".name_hove2")
+    doc.style.visibility="hidden";
+    doc.style.opacity="0";
 }
 
-function hideCard3(){
-    var doc = document.getElementById("display-card3")
-    doc.style.top = "100px";
-    doc.style.visibility = "hidden";
-    doc.style.opacity = "0";
-    // console.log("Hello")
+function displayImageCard3(){
+    var doc=document.getElementsByClassName(".name_hove3");
+    doc.style.visibility="visible";
+    doc.style.opacity="1";
+}
+function hideImageCard3(){
+    var doc=document.getElementsByClassName(".name_hove3")
+    doc.style.visibility="hidden";
+    doc.style.opacity="0";
 }
 
-function displayCard4(){
-    var doc = document.getElementById("display-card4")
-    doc.style.top = "133px";
-    doc.style.visibility = "visible";
-    doc.style.opacity = "1";
+function displayImageCard4(){
+    var doc=document.getElementsByClassName(".name_hove4");
+    doc.style.visibility="visible";
+    doc.style.opacity="1";
+}
+function hideImageCard4(){
+    var doc=document.getElementsByClassName(".name_hove4")
+    doc.style.visibility="hidden";
+    doc.style.opacity="0";
 }
 
-function hideCard4(){
-    var doc = document.getElementById("display-card4")
-    doc.style.top = "100px";
-    doc.style.visibility = "hidden";
-    doc.style.opacity = "0";
-    // console.log("Hello")
+function displayImageCard5(){
+    var doc=document.getElementsByClassName(".name_hove5");
+    doc.style.visibility="visible";
+    doc.style.opacity="1";
+}
+function hideImageCard5(){
+    var doc=document.getElementsByClassName(".name_hove5")
+    doc.style.visibility="hidden";
+    doc.style.opacity="0";
 }
 
-function displayCard5(){
-    var doc = document.getElementById("display-card5")
-    doc.style.top = "133px";
-    doc.style.visibility = "visible";
-    doc.style.opacity = "1";
-}
-
-function hideCard5(){
-    var doc = document.getElementById("display-card5")
-    doc.style.top = "100px";
-    doc.style.visibility = "hidden";
-    doc.style.opacity = "0";
-    // console.log("Hello")
-}
-
-function displayCard6(){
-    var doc = document.getElementById("display-card6")
-    doc.style.top = "133px";
-    doc.style.visibility = "visible";
-    doc.style.opacity = "1";
-}
-
-function hideCard6(){
-    var doc = document.getElementById("display-card6")
-    doc.style.top = "100px";
-    doc.style.visibility = "hidden";
-    doc.style.opacity = "0"
-    // console.log("Hello")
-}
-
-function displayCard7(){
-    var doc = document.getElementById("display-card7")
-    doc.style.top = "133px";
-    doc.style.visibility = "visible";
-    doc.style.opacity = "1";
-}
-
-function hideCard7(){
-    var doc = document.getElementById("display-card7")
-    doc.style.top = "100px";
-    doc.style.visibility = "hidden";
-    doc.style.opacity = "0";
-    // console.log("Hello")
-}
-
-function displayCard8(){
-    var doc = document.getElementById("display-card8")
-    doc.style.top = "133px";
-    doc.style.visibility = "visible";
-    doc.style.opacity = "1";
-}
-
-function hideCard8(){
-    var doc = document.getElementById("display-card8")
-    doc.style.top = "100px";
-    doc.style.visibility = "hidden";
-    doc.style.opacity = "0";
-    // console.log("Hello")
-}
+// function clicked(){
+//     var element = document.getElementById('next');
+//     element.scrollLeft =  element.scrollLeft + 30 
+// }
