5482507bbc62ca16174945940d0ca46654082d58
Vasu Tanneru <vasutanneru035@gmail.com>
2024-04-15 00:11:14 +0530

    0.0.9

--- script.js
+++ script.js
@@ -157,7 +157,34 @@ function displayImageCard(val){
 
 function clicked(){
     var element = document.getElementById('next_slide');
-    element.scrollLeft =  element.scrollLeft + 30 
+    element.scrollLeft =  element.scrollLeft + 200 
     console.log(element.scrollLeft)
 
+}
+
+function prevClicked(){
+    var element = document.getElementById('next_slide');
+    element.scrollLeft =  element.scrollLeft - 200 
+    console.log(element.scrollLeft)
+
+}
+
+// Get the button
+var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
